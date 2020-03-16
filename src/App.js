25c139583ee498fdef02dcfb8cc97b287e1473e0
xiaocaoer <1608708253@qq.com>
2020-03-16 14:18:44 +0800

    评论管理案例完成
    Signed-off-by: xiaocaoer <1608708253@qq.com>

--- src/App.js
+++ src/App.js
@@ -16,6 +16,25 @@ export default class App extends Component {
     //将更改后的状态设置回去
     this.setState({comments})
   }
+  deleteComment = (id) => {
+    //排除是这个id的对象，使用数组filter方法
+    // let comments = this.state.comments.filter((commentObj) => {
+    //   return commentObj.id !==id
+    // })
+    //方法二：使用数组findIndex方法,这里得到的是id是参数id的数组元素的下标，要删除该下标的数组元素
+    let result =  this.state.comments.findIndex((commentObj) => {
+      return commentObj.id = id
+    })
+    let {comments} = this.state
+    // comments[result] = undefined.删除数组中为某下标的元素不能这么写，这个只是让这一项的值变为undefined，
+    //正常的删除还要让后面的元素往前移动
+    //知道下标，采用数组方法splice(删除起始位置，删几位)
+    comments.splice(result,1)
+    this.setState({comments})
+  }
+
+  //删除对象中的某个属性，delete obj.name
+  //删除数组中某下标的元素,arr.splice(删除起始位置，删除位数)，影响原数组
 
   render() {
     let {comments} = this.state
@@ -32,7 +51,7 @@ export default class App extends Component {
         </header>
         <div className="container">
           <Add  changeState={this.changeState} />
-          <Comment comments={comments}/>
+          <Comment comments={comments} deleteComment={this.deleteComment} />
         </div>
       </div>
     )
--- src/components/comment/comment.jsx
+++ src/components/comment/comment.jsx
@@ -4,7 +4,7 @@ import './comment.css'
 
 export default class Comment extends Component {
   render() {
-    let {comments} = this.props
+    let {comments,deleteComment} = this.props
     // console.log( this.props)
     //根据comments数组的长度生成相对应数目的Item组件,而数组有多长，
     //map的回调就会自动走多少次，最终的返回结果是这些回调每次返回结果的组合，
@@ -14,10 +14,13 @@ export default class Comment extends Component {
     return (
       <div className="col-md-8">
         <h3 className="reply">评论回复：</h3>
-        <h2 style={{ display: 'none' }}>暂无评论，点击左侧添加评论！！！</h2>
+        <h2 style={{ display: comments.length ? 'none' : 'block'}}>暂无评论，点击左侧添加评论！！！</h2>
         <ul className="list-group">
+          {/* //注意：map方法不会对空数组进行检测，所以当下面的数组为空是下面的代码不会执行，就不会报错 */}
           {comments.map((commentObj) => {
-            return <Item key={commentObj.id} name={commentObj.name} content={commentObj.content} />
+            //这里因为commentObj中的每个属性都是要做在Item中用的到,所以使用ES8三点运算符在React中的特殊用法,解包一个对象
+            return <Item key={commentObj.id} {...commentObj} deleteComment={deleteComment} />
+            //...commentObj等价于写了三组标签属性:id={commentObj.id} name={commentObj.name} content={commentObj.content}
           })}
         </ul>
       </div>
